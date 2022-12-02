694ba264a23340dc2f8af3c525e92c4541bfd1b3
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2022-12-02 19:01:02 -0300

    CRUD Cliente Completo

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,138 @@
 const express = require("express");
 const app = express();
+const mysql = require('mysql2');
+const cors = require("cors");
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'rodrigo',
+    password: 'fatec',
+    database: 'wb_database'
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/novo-cliente', (req, res) => {
+    const cliente = req.body;
+
+    let sql = 'INSERT INTO clientes (nome, nomeSocial, genero, cpfValor, cpfData, dataCadastro) values (?, ?, ?, ?, ?, ?)'
+
+    let data = new Date().toISOString().split('T')[0];
+    db.query(
+        sql,
+        [cliente.nome, cliente.nomeSocial, cliente.genero, cliente.cpf.valor, cliente.cpf.data, data],
+        (err, result) => {
+            if(err) {
+                console.log(err);
+            } else {
+                const id = result.insertId;
+                cliente.rgList.forEach(rg => {
+                    let sql = 'INSERT INTO rgs (valor, data, cliente) VALUES (?, ?, ?)'
+                    db.query(
+                        sql,
+                        [rg.valor, rg.data, id], (err, result) => {
+                            if(err) console.log(err)
+                        }
+                    )
+                })
+                cliente.telefoneList.forEach(telefone => {
+                    let sql = 'INSERT INTO telefones (telefone, cliente) VALUES (?, ?)'
+                    db.query(
+                        sql,
+                        [telefone, id],
+                        (err, result) => {
+                            if(err) console.log(err)
+                        }
+                    )
+                })
+            }
+            res.send(result)
+        }
+    )
+});
+
+app.get('/clientes', (req, res) => {
+    let sql = 'select id, nome, dataCadastro, genero from clientes';
+    db.query(sql, (err, result) => {
+        if(err) console.log(err);
+        else res.send(result)
+    });
+});
+
+app.get('/cliente', (req, res) => {
+    const { id } = req.query;
+
+    let sql = 'select nome, nomeSocial, genero, cpfValor, cpfData from clientes where id = ?'
+    db.query(sql, [id], (err, cliente) => {
+        if(err) console.log(err);
+        else {
+            const { nome, nomeSocial, genero, cpfValor, cpfData } = cliente[0];
+
+            let sql = 'select data, valor from rgs where cliente = ?'
+            db.query(sql, [id], (err, rgs) => {
+                let sql = 'select telefone from telefones where cliente = ?';
+                db.query(sql, [id], (err, telefones) => {
+                    res.send({
+                        nome,
+                        nomeSocial,
+                        genero,
+                        cpf: {
+                            valor: cpfValor,
+                            data: cpfData
+                        },
+                        rgs,
+                        telefones: telefones
+                    })
+                })
+            })
+        }
+    })
+});
+
+app.put('/editar-cliente', (req, res) => {
+    const {id, nome, nomeSocial, genero, cpf} = req.body
+    
+    let sql = 'UPDATE clientes SET nome = ?, nomeSocial = ?, genero = ?, cpfValor = ?, cpfData = ? where id = ?'
+    db.query(sql, [nome, nomeSocial, genero, cpf.valor, cpf.data, id], (err, result) => {
+        if(err) console.log(err)
+        else {
+            let sqlRG = 'DELETE FROM rgs WHERE cliente = ?'
+            db.query(sqlRG, [id], (err) => console.log(err))
+
+                const {rgs} = req.body;
+                let sqlRGNEW = 'INSERT INTO rgs (valor, data, cliente) VALUES (?, ?, ?)'
+                rgs.forEach(rg => {
+                    db.query(sqlRGNEW, [rg.valor, rg.data, id], (err) => {if(err) console.log(err)})
+                });
+            
+            let sqlTEL = 'DELETE FROM telefones WHERE cliente = ?'
+            db.query(sqlTEL, [id], (err) => console.log(err))
+                const {telefones} = req.body;
+                let sqlTELNEW = 'INSERT INTO telefones (telefone, cliente) VALUES (?, ?)'
+                telefones.forEach(telefone => {
+                    db.query(sqlTELNEW, [telefone, id], (err) => {if(err) console.log(err)});
+                });
+                res.send(JSON.stringify({result: 'ok'}))
+        }
+    })
+});
+
+app.delete('/deletar-cliente', (req, res) => {
+    const { id } = req.query;
+
+    let sql = 'DELETE FROM clientes WHERE id = ?'
+    db.query(sql, [id], (err, result) => {
+        if(err) console.log(err)
+        else res.send(result)
+        console.log('deletando...')
+    });
+})
 
 app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
-app.listen(3001, () => {
+app.listen(4001, () => {
     console.log('Rodando servidor');
 });
